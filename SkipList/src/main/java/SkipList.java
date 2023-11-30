9555874bfb25b4bb0c08999cff4895c8b277691f
zzunho9 <jamlove4763@naver.com>
2023-11-30 20:03:12 +0900

    Chore: 필요 없는 주석 삭제

--- SkipList/src/main/java/SkipList.java
+++ SkipList/src/main/java/SkipList.java
@@ -10,23 +10,18 @@ public class SkipList<K extends Comparable<K>, V> {
         Node<K, V> node = head;
         Node<K, V> next;
 
-        // 우측 비교. 언제까지? => level == 1 이고 다음 값이 null 일 떄 까지.
         while (node.getLevel() != 1 && node.hasNext()) {
             next = node.getNext();
 
-            // key 보다 작은 값이 다음에 있을 경우 => 다음으로 이동.
-            // 언제까지? => 다음 값이 key 보다 크거나 같을 때 까지.
             while(next != null && lessThanOrEqual(key, next.getKey())) {
                 node = next;
                 next = node.getNext();
             }
 
-            // key와 현제 노드가 같은 경우
             if (node.getKey().compareTo(key) == 0) {
                 break;
             }
 
-            // 없을 경우 => 아래로 이동
             node = node.getDown();
         }
 
